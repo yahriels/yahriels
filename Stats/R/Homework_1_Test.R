7018c2ff8809e8feec717282f819558671bfc726
Yahriel Salinas-Reyes <97989661+yahriels@users.noreply.github.com>
2024-08-27 16:20:40 +0000

    update now

---
+++ Stats/R/Homework_1_Test.R
@@ -0,0 +1,17 @@
+
+# Question 1
+birthweight <- c(3600,1700,4000,3900,3100,3800,2200,3000)
+summary(birthweight)
+sd(birthweight)
+hist(birthweight)
+
+# Question 2
+Ar_rice <- c(6.1, 5.4, 6.9, 4.9, 6.6, 6.3, 6.7, 8.2, 7.8, 1.5, 5.4, 7.3)
+hist(Ar_rice, 
+     main = "Measured amount of arsenic in brown rice", 
+     # xlab = ""
+     ylab = "Amount (ug/serving)",
+     breaks = 2)
+# Question
+
+
