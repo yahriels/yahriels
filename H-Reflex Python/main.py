e74cc7a60631d9af84360b98053e1627de77cc82
Yahriel Salinas-Reyes <yahriels@iastate.edu>
2024-10-01 14:09:07 +0000

    GUI stuff

--- H-Reflex Python/main.py
+++ H-Reflex Python/main.py
@@ -1,23 +1,24 @@
 # import sys
 import sys
 
-# Import UI File
-from interface import *
 
-# create main window
+# Import UI File (assuming interface.py has a class Ui_MainWindow)
+from interface import * 
+
+# Create main window
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
-            QMainWindow.__init__(self)
-            
-            self.ui = Ui_MainWindow()
-            self.ui.setupUI(self)
-            
-            self.show()
-            
-# execute file
-
+        QMainWindow.__init__(self)
+        
+        self.ui - Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        self.show()
+        
+        
+# Execute File
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
